07cf98d8ada86405d225d52df792d2199dac20ee
3PIV <pprovins@vols.utk.ed>
2019-07-29 09:56:51 -0500

    Updated Scene to have OBJ paths

--- redo/scene.sh
+++ redo/scene.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+scenes=/mnt/seenas2/data/intelScenes/Scenes
+abstract=/mnt/seenas2/data/intelScenes/Scenes/Abstract
+nature=/mnt/seenas2/data/intelScenes/Scenes/Nature
+city=/mnt/seenas2/data/intelScenes/Scenes/City
 
 exec /opt/ospray-1.8.5/build/ospExampleViewer
