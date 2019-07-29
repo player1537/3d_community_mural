3e64aa5e2d26eafafbbad781a6445b3a1d22fbd1
3PIV <pprovins@vols.utk.ed>
2019-07-29 16:47:52 -0500

    Made Camera Move

--- redo/ospExampleViewer.cpp
+++ redo/ospExampleViewer.cpp
@@ -18,18 +18,31 @@
 #include "sg/geometry/TriangleMesh.h"
 
 #include "ospapp/OSPApp.h"
-//#include "widgets/imguiViewer.h"
+#include "widgets/imguiViewer.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "sg/3rdParty/stb_image_write.h"
 
 #include "stdlib.h"
 
+void eraseSubStr(std::string &mainStr, const std::string &toErase)
+{
+  // Search for the substring in string
+  size_t pos = mainStr.find(toErase);
+
+  if (pos != std::string::npos) {
+    // If found then erase it from string
+    mainStr.erase(pos, toErase.length());
+  }
+}
+
 namespace ospray {
   namespace app {
 
-    struct Foo {
-      void write(void *data, int size) {
+    struct ContextCaptureStruct
+    {
+      void write(void *data, int size)
+      {
         unsigned char *d = (unsigned char *)data;
         bytes.insert(bytes.end(), d, d + size);
       }
@@ -37,155 +50,10 @@ namespace ospray {
       std::vector<unsigned char> bytes;
     };
 
-    void write(void *context, void *data, int size) {
-      Foo *myfoo = (Foo *)context;
+    void jpgWriteHelp(void *context, void *data, int size)
+    {
+      ContextCaptureStruct *myfoo = (ContextCaptureStruct *)context;
       myfoo->write(data, size);
-    }
-
-    OSPGeometry
-    makeOBJGeometry(const char *filename) {
-      OSPGeometry geometry;
-      OSPData vertex_data, index_data;
-      int nvertex, nindex;
-      float *vertices;
-      int *indices;
-      FILE *f;
-      char c;
-      size_t sz;
-      int has_vertex, has_index, has_extra1, has_extra2;
-      
-      f = fopen(filename, "rb");
-      
-      (void)fread(&c, sizeof(c), 1, f);
-      has_vertex = c == 'V';
-      
-      (void)fread(&c, sizeof(c), 1, f);
-      has_index = c == 'I';
-      
-      (void)fread(&c, sizeof(c), 1, f);
-      has_extra1 = c == '?';
-      
-      (void)fread(&c, sizeof(c), 1, f);
-      has_extra2 = c == '?';
-    
-      if (!has_vertex) goto error;
-      if (!has_index) goto error;
-      if (has_extra1) goto error;
-      if (has_extra2) goto error;
-      
-      vertices = NULL;
-      if (has_vertex) {
-        (void)fread(&nvertex, sizeof(nvertex), 1, f);
-        sz = (size_t)nvertex * 3;
-        vertices = (float *)malloc(sz * sizeof(*vertices));
-        (void)fread(vertices, sizeof(*vertices), sz, f);
-        vertex_data = ospNewData(nvertex, OSP_FLOAT3, vertices, OSP_DATA_SHARED_BUFFER);
-        ospCommit(vertex_data);
-      }
-      
-      indices = NULL;
-      if (has_index) {
-        (void)fread(&nindex, sizeof(nindex), 1, f);
-        sz = (size_t)nindex * 3;
-        indices = (int *)malloc(sz * sizeof(*indices));
-        (void)fread(indices, sizeof(*indices), sz, f);
-        index_data = ospNewData(nindex, OSP_INT3, indices, OSP_DATA_SHARED_BUFFER);
-        ospCommit(index_data);
-      }
-      
-      geometry = ospNewGeometry("triangles");
-      if (has_vertex) {
-        ospSetData(geometry, "vertex", vertex_data);
-        ospRelease(vertex_data);
-      }
-      if (has_index) {
-        ospSetData(geometry, "index", index_data);
-        ospRelease(index_data);
-      }
-      ospCommit(geometry);
-      
-    error:
-      fclose(f);
-      
-      return geometry;
-    }
-    
-    
-    OSPMaterial *
-    makePreloadedMaterials(void) {
-      OSPMaterial *materials, material;
-      int nmaterial;
-      char *s, name[32];
-      int i;
-      float Kdr, Kdg, Kdb, d, Ksr, Ksg, Ksb, Ns;
-      
-      s = getenv("nmat");
-      if (s == NULL) {
-        nmaterial = 0;
-      } else if (sscanf(s, "%d", &nmaterial) != 1) {
-        return NULL;
-      }
-      
-      materials = (OSPMaterial *)malloc(nmaterial * sizeof(*materials));
-      
-      for (i=0; i<nmaterial; ++i) {
-        snprintf(name, sizeof(name), "mat_%d", i);
-        s = getenv(name);
-        if (sscanf(s, "%*s %f %f %f %f %f %f %f %f", &Kdr, &Kdg, &Kdb, &d, &Ksr, &Ksg, &Ksb, &Ns) != 8) {
-          return NULL;
-        }
-        
-        material = ospNewMaterial2("pathtracer", "OBJMaterial");
-        ospSet3f(material, "Kd", Kdr, Kdg, Kdb);
-        ospSet1f(material, "d", d);
-        ospSet3f(material, "Ks", Ksr, Ksg, Ksb);
-        ospSet1f(material, "Ns", Ns);
-        ospCommit(material);
-        
-        materials[i] = material;
-      }
-      
-      return materials;
-    }
-    
-    
-    OSPGeometry *
-    makePreloadedGeometries(OSPModel **out_models) {
-      OSPGeometry *geometries, geometry;
-      OSPModel *models, model;
-      int i, nobj;
-      char *s, name[32];
-      
-      s = getenv("nobj");
-      if (s == NULL) {
-        nobj = 0;
-      } else if (sscanf(s, "%d", &nobj) != 1) {
-        return NULL;
-      }
-      
-      geometries = (OSPGeometry *)malloc(nobj * sizeof(*geometries));
-      models = (OSPModel *)malloc(nobj * sizeof(*models));
-      
-      for (i=0; i<nobj; ++i) {
-        snprintf(name, sizeof(name), "obj_%d", i);
-        s = getenv(name);
-        
-        // We get a string like "Name Path" and we want the
-        // path to load the object
-        s = strrchr(s, ' ') + 1;
-        
-        geometry = makeOBJGeometry(s);
-        
-        model = ospNewModel();
-        ospAddGeometry(model, geometry);
-        ospCommit(model);
-        
-        geometries[i] = geometry;
-        models[i] = model;
-      }
-      
-      *out_models = models;
-      return geometries;
     }
 
     class OSPExampleViewer : public OSPApp
@@ -193,60 +61,217 @@ namespace ospray {
       void render(const std::shared_ptr<sg::Frame> &) override;
       int parseCommandLine(int &ac, const char **&av) override;
 
-      std::string fileName {"./aout.jpeg"};
+      std::string fileName{"./aout.jpeg"};
     };
 
     void OSPExampleViewer::render(const std::shared_ptr<sg::Frame> &root)
     {
-      FILE *f = fdopen(100, "w");
-      sg::Node &camera = root->child("camera");
-      sg::Node &renderer = root->child("renderer");
-      sg::Node &model = renderer.child("world");
-      for (;;) {
+      // Establish the Camera
+      auto &camera     = root->child("camera");
+      camera["pos"]    = vec3f(0.0f, 3.5f, 10.2f);
+      camera["up"]     = vec3f(0.0f, 1.0f, 0.0f);
+      camera["dir"]    = vec3f(0.0f, 0.0f, -1.0f);
+      camera["fovy"]   = 90.0f;
+      camera["aspect"] = 1.0f;
+      camera.commit();
 
-        camera["pos"] = vec3f(0.0f, 3.5f, 10.2f);
-        camera["up"] = vec3f(0.0f, 1.0f, 0.0f);
-        camera["dir"] = vec3f(0.0f, 0.0f, -1.0f);
-        camera["fovy"] = 90.0f;
+      // Retrieve the Renderer's Children
+      auto &renderer = root->child("renderer");
+      auto &world    = renderer.child("world");
+      // Skip the World's Nodes that are not active actors in the scene
+      std::vector<std::string> nodesToSkip{
+          "bounds", "compactMode", "dynamicScene", "robustMode"};
+
+      // Find the Active actors and store them separately
+      std::map<std::string, std::shared_ptr<sg::Node>> sceneActors;
+      for (auto &element : world.children()) {
+        if ((std::find(
+                nodesToSkip.begin(), nodesToSkip.end(), element.first)) ==
+            nodesToSkip.end()) {
+          printf("%s\n", element.first.c_str());
+          sceneActors.emplace(std::make_pair(element.first, element.second));
+        }
+      }
+
+      /**
+       * at this point the actors have all been found.
+       * We cannot change the material here. It is fixed at load time.
+       * What we can do with them:
+       *    change rotation (vec3f)
+       *    change position (vec3f)
+       *    change scale    (vec3f)
+       * We can also duplicate Nodes. Do this by adding them back to the parent,
+       * a variable named world, using a different name
+       */
+      std::map<std::string, std::shared_ptr<sg::Node>> namedActors;
+      for (auto &actor : sceneActors) {
+        std::string actorName = actor.second->name();
+        eraseSubStr(actorName, "transform_");
+        eraseSubStr(actorName, "_0_0_0");
+        printf("actor: %s as %s\n", actor.first.c_str(), actorName.c_str());
+        namedActors.emplace(std::make_pair(actorName, actor.second));
+        actor.second->child("position").setValue(vec3f(0.0f, 0.0f, 0.0f));
+      }
+
+      FILE *input, *info, *error, *output;
+      input = fopen("/dev/stdin", "r");
+      info = fopen("/dev/stdout", "w");
+      error = fopen("/dev/stderr", "w");
+      output = fopen("/dev/fd/100", "wb");
+      while (true) {
+
+        float camPosX, camPosY, camPosZ, camUpX, camUpY, camUpZ, camVuDirX, camVuDirY, camVuDirZ;
+        int quality;
+        float bx1, by1, bz1, bsx1, bsy1, bsz1;
+        int matid1, objid1;
+        float bx2, by2, bz2, bsx2, bsy2, bsz2;
+        int matid2, objid2;
+        float bx3, by3, bz3, bsx3, bsy3, bsz3;
+        int matid3, objid3;
+        float negxr, negxg, negxb;
+        float posxr, posxg, posxb;
+        float negyr, negyg, negyb;
+        float posyr, posyg, posyb;
+        float negzr, negzg, negzb;
+        float poszr, poszg, poszb;
+        float lightx, lighty, lightz;
+        float lightr, lightg, lightb, lightint;
+
+        fprintf(info, "Waiting for request...\n");
+
+        if (fscanf(input, "%f %f %f %f %f %f %f %f %f %d", &camPosX, &camPosY, &camPosZ, &camUpX, &camUpY, &camUpZ, &camVuDirX, &camVuDirY, &camVuDirZ, &quality) != 10) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        if (fscanf(input, "%f %f %f %f %f %f %d %s", &bx1, &by1, &bz1, &bsx1, &bsy1, &bsz1, &matid1, &objid1) != 8) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        if (fscanf(input, "%f %f %f %f %f %f %d %s", &bx2, &by2, &bz2, &bsx2, &bsy2, &bsz2, &matid2, &objid2) != 8) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        if (fscanf(input, "%f %f %f %f %f %f %d %s", &bx3, &by3, &bz3, &bsx3, &bsy3, &bsz3, &matid3, &objid3) != 8) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        /// IGNORE VALS FROM HERE
+        /**
+         *
+         *
+         */
+        if (fscanf(input, "%f %f %f", &negxr, &negxg, &negxb) != 3) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        if (fscanf(input, "%f %f %f", &negyr, &negyg, &negyb) != 3) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        if (fscanf(input, "%f %f %f", &negzr, &negzg, &negzb) != 3) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        if (fscanf(input, "%f %f %f", &posxr, &posxg, &posxb) != 3) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        if (fscanf(input, "%f %f %f", &posyr, &posyg, &posyb) != 3) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        if (fscanf(input, "%f %f %f", &poszr, &poszg, &poszb) != 3) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+        /// TO HERE FOR NOW
+        /*
+         *
+         *
+         */
+
+        if (fscanf(input, "%f %f %f %f %f %f %f", &lightx, &lighty, &lightz, &lightr, &lightg, &lightb, &lightint) != 7) {
+          fprintf(error, "Error: bad format\n");
+          fprintf(output, "9:error arg,");
+          fflush(output);
+          continue;
+        }
+
+        fprintf(info, "Got request\n");
+
+
+        camera["pos"]    = vec3f(camPosX, camPosY, camPosZ);
+        camera["up"]     = vec3f(camUpX, camUpY, camUpZ);
+        camera["dir"]    = vec3f(camVuDirX, camVuDirY, camVuDirZ);
+        camera["fovy"]   = 90.0f;
         camera["aspect"] = 1.0f;
         camera.commit();
-  
+
+        // Render a single Frame
         std::shared_ptr<sg::FrameBuffer> fb = std::make_shared<sg::FrameBuffer>(vec2i(512, 512));
         root->setChild("frameBuffer", fb);
         root->setChild("navFrameBuffer", fb);
-  
-        renderer["spp"] = 1;
+        renderer["spp"]                            = 20;
         std::shared_ptr<sg::FrameBuffer> fbCapture = root->renderFrame(true);
-        auto fbSize = fbCapture->size();
-  
-        auto fbColour = (vec4uc*)fbCapture->map(OSP_FB_COLOR);
-  
-  
+        auto fbSize                                = fbCapture->size();
+        auto fbColour = (vec4uc *)fbCapture->map(OSP_FB_COLOR);
+
+        // Capture the rendered frame to a buffer
         std::vector<unsigned char> rgbAsUnsChars;
         int index = 0;
-        for(int row = fbSize.y - 1; row >= 0; --row){
-          for(int col = 0; col < fbSize.x; ++col) {
+        for (int row = fbSize.y - 1; row >= 0; --row) {
+          for (int col = 0; col < fbSize.x; ++col) {
             index = row * fbSize.x + col;
             rgbAsUnsChars.push_back((unsigned char)(fbColour[index].x));
             rgbAsUnsChars.push_back((unsigned char)(fbColour[index].y));
             rgbAsUnsChars.push_back((unsigned char)(fbColour[index].z));
           }
         }
-  
-  
-        Foo myfoo;
-        int ret = stbi_write_jpg_to_func(write, (void*)&myfoo, fbSize.x, fbSize.y, 3, &rgbAsUnsChars[0], 100);
+
+        ContextCaptureStruct captureStruct;
+        int ret = stbi_write_jpg_to_func(jpgWriteHelp, (void*)&captureStruct, fbSize.x, fbSize.y, 3, &rgbAsUnsChars[0], 100);
         if (!ret) {
           printf("bad\n");
+          break;
         }
-  
-        fprintf(f, "%lu:", myfoo.bytes.size());
-        fwrite(myfoo.bytes.data(), 1, myfoo.bytes.size(), f);
-        fprintf(f, ",");
-        fflush(f);
+
+        fprintf(output, "%lu:", captureStruct.bytes.size());
+        fwrite(captureStruct.bytes.data(), 1, captureStruct.bytes.size(), output);
+        fprintf(output, ",");
+        fflush(output);
+        fbCapture->clear();
       }
-      fclose(f);
-    }
+      fclose(output);
+    }  // namespace app
 
     int OSPExampleViewer::parseCommandLine(int &ac, const char **&av)
     {
@@ -261,8 +286,8 @@ namespace ospray {
       return 0;
     }
 
-  } // ::ospray::app
-} // ::ospray
+  }  // namespace app
+}  // namespace ospray
 
 int main(int ac, const char **av)
 {
